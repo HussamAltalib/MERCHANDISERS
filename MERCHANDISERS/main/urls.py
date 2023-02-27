f9e4ef88191f6bee1e713ecdd6b739520f35c97b
HussamAltalib <hussamaltalib@outlook.com>
2023-02-27 12:23:28 +0300

    add voting feature fo answers

--- MERCHANDISERS/main/urls.py
+++ MERCHANDISERS/main/urls.py
@@ -7,14 +7,15 @@ urlpatterns = [
        path("", views.index, name="index_page"),
        path("ask/question/", views.ask_question_page, name="ask_question_page"),
        path("answers/details/<question_id>/", views.answers_details_page, name="answers_details_page"),
+       path("answers/details/<question_id>/<answer_id>/", views.answers_details_page2, name="answers_details_page2"),
 
        path("my/profile/", views.my_profile, name="my_profile_page"),
        path("my/questions/", views.my_questions, name="my_questions_page"),
        path("my/answers/", views.my_answers, name="my_answers_page"),
 
-       path("profile/<user_id>/", views.profile, name="profile_page"),
-       path("questions/<user_id>/", views.questions, name="questions_page"),
-       path("answers/<user_id>/", views.answers, name="answers_page"),
+       path("others/profile/<user_id>/", views.others_profile, name="others_profile_page"),
+       path("others/questions/<user_id>/", views.others_questions, name="others_questions_page"),
+       path("others/answers/<user_id>/", views.others_answers, name="others_answers_page"),
 
        path("delete/question/<question_id>/", views.delete_question, name="delete_question"),
        path("edit/question/<question_id>/", views.edit_question, name="edit_question_page"),
@@ -24,9 +25,11 @@ urlpatterns = [
 
        path("upgrade/question/<question_id>/", views.upgrade_question, name="upgrade_question"),
        path("downgrade/question/<question_id>/", views.downgrade_question, name="downgrade_question"),
-       path("cancel/my/vote/<question_id>/", views.cancel_my_vote, name="cancel_my_vote"),
+       path("cancel/question/vote/<question_id>/", views.cancel_question_vote, name="cancel_question_vote"),
 
-       path("upgrade/answer/<answer_id>/", views.upgrade_answer, name="upgrade_answer"),
+       path("upgrade/answer/<question_id>/<answer_id>/", views.upgrade_answer, name="upgrade_answer"),
+       path("downgrade/answer/<question_id>/<answer_id>/", views.downgrade_answer, name="downgrade_answer"),
+       path("cancel/answer/vote/<question_id>/<answer_id>/", views.cancel_answer_vote, name="cancel_answer_vote"),
 
 
 ]
